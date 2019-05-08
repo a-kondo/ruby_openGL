845c290e194f56d451fb9d572a293b23d8b0a267
a-kondo <akihiro.kondo.ak@gmail.com>
2019-05-08 22:18:24 +0900

    add function to read map

--- sample/sample.rb
+++ sample/sample.rb
@@ -85,6 +85,103 @@ class Cube
   end
 end
 
+class Map
+  attr_reader :db_path
+
+  def initialize(file_path)
+    @db_path = file_path
+    sql = "SELECT MIN_LATITUDE, MIN_LONGITUDE, MAX_LATITUDE, MAX_LONGITUDE from METADATA"
+    ret = `sqlite3 #{@db_path} "#{sql}"`
+    min_lat, min_lng, max_lat, max_lng = ret.chop!.split('|')
+    puts "border"
+    @min_lat = organize_accuracy_to_i(min_lat)
+    @min_lng = organize_accuracy_to_i(min_lng)
+    @max_lat = organize_accuracy_to_i(max_lat)
+    @max_lng = organize_accuracy_to_i(max_lng)
+    puts "min: #{@min_lat} #{@min_lng}"
+    puts "----------"
+
+
+    sql = "SELECT LATITUDE from node order by LATITUDE limit 1"
+    ret = `sqlite3 #{@db_path} "#{sql}"`
+    @min_lat2 = organize_accuracy_to_i(ret.chop)
+
+    sql = "SELECT LONGITUDE from node order by LONGITUDE limit 1"
+    ret = `sqlite3 #{@db_path} "#{sql}"`
+    @min_lng2 = organize_accuracy_to_i(ret.chop)
+
+    puts "min ___ #{@min_lat2} #{@min_lng2}"
+
+    @links = []
+  end
+
+  def organize_accuracy_to_i(str)
+    i, f = str.split('.')
+
+    for i in 1..7-f.length
+      str << '0'
+    end
+
+    return str.delete('.').to_i()
+  end
+
+  def read()
+    # get all node infomation to hash
+    sql = "select LATITUDE, LONGITUDE, ID from NODE"
+    ret = `sqlite3 #{@db_path} "#{sql}"`
+    nodes = ret.split("\n")
+
+    node_hash = {}
+    nodes.each do |node|
+      lat, lng, id = node.split('|')
+      node_hash[id] = {lat: lat, lng: lng}
+    end
+
+
+    sql = "select START_NODE_ID, END_NODE_ID from LINE"
+    ret = `sqlite3 #{@db_path} "#{sql}"`
+    lines = ret.split("\n")
+
+    lines.each do |line|
+      start_node_id, end_node_id = line.split('|')
+
+      # start node 
+      s_node_lat = organize_accuracy_to_i(node_hash[start_node_id][:lat])
+      s_node_lng = organize_accuracy_to_i(node_hash[start_node_id][:lng])
+
+      lat = s_node_lat - @min_lat2
+      lng = s_node_lng - @min_lng2
+      # lat /= 17000.0
+      # lng /= 107000.0
+      lat /= 20000.0
+      lng /= 20000.0
+      s_point = [lat, lng, 0]
+
+      # end node
+      e_node_lat = organize_accuracy_to_i(node_hash[end_node_id][:lat])
+      e_node_lng = organize_accuracy_to_i(node_hash[end_node_id][:lng])
+
+      lat = e_node_lat - @min_lat2
+      lng = e_node_lng - @min_lng2
+      # lat /= 17000.0
+      # lng /= 107000.0
+      lat /= 20000.0
+      lng /= 20000.0
+      e_point = [lat, lng, 0]
+
+      #puts "point: #{s_point} #{e_point}"
+      @links.push([s_point, e_point])
+    end
+  end
+
+  def draw()
+    @links.each do |link|
+      l = Link.new(link[0], link[1])
+      l.draw()
+    end
+  end
+end
+
 class Sample 
   def axises
     GL.PointSize(5)
@@ -112,7 +209,9 @@ class Sample
   def disp()
     GL.Clear(GL::COLOR_BUFFER_BIT)
     GL.LoadIdentity()
-    GLU.LookAt(@view_from_x, @view_from_y, @view_from_z, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+    # GLU.LookAt(@view_from_x, @view_from_y, @view_from_z, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+    GLU.LookAt(@view_from_x, @view_from_y, @view_from_z,
+               @view_to_x, @view_to_y, @view_to_z, 0.0, 1.0, 0.0)
 
     axises()
 
@@ -123,8 +222,10 @@ class Sample
     GL.Translated(1.0, 1.0, 1.0)
     GL.Rotated(@r*2, 0.0, 1.0, 0.0)
     Cube.draw(Color::RED)
-
     GL.PopMatrix()
+
+    @map.draw()
+
     GLUT.SwapBuffers()
   end
 
@@ -139,7 +240,7 @@ class Sample
 
     GL.MatrixMode(GL::PROJECTION)
     GL.LoadIdentity()
-    GLU.Perspective(30.0, w/h, 1.0, 100.0)
+    GLU.Perspective(30.0, w/h, 1.0, 300.0)
 
     GL.MatrixMode(GL::MODELVIEW)
   end
@@ -175,6 +276,7 @@ class Sample
     case key
     when 'q'
       exit()
+    
     when 'a'
       if @rotate_flag
         GLUT.IdleFunc(lambda {})
@@ -186,26 +288,64 @@ class Sample
     when 'w'
       @r += 1
       GLUT.PostRedisplay()
-    when 'k'
+    
+    when 'i'
       view_from_move_to_init()
+      print_view_position()
       GLUT.PostRedisplay()
-    when 'j'
+    when 'h'
       @view_from_x -= 2.0
+      @view_to_x = @view_from_x
+      print_view_position()
       GLUT.PostRedisplay()
     when 'l'
       @view_from_x += 2.0
+      @view_to_x = @view_from_x
+      print_view_position()
       GLUT.PostRedisplay()
-    when 'i'
+    when 'k'
       @view_from_y += 2.0
+      @view_to_y = @view_from_y
+      print_view_position()
       GLUT.PostRedisplay()
-    when 'm'
+    when 'j'
       @view_from_y -= 2.0
+      @view_to_y = @view_from_y
+      print_view_position()
       GLUT.PostRedisplay()
+    
     when 's'
+      @view_from_z += 10.0
+      print_view_position()
+      GLUT.PostRedisplay()
+    when 'S'
       @view_from_z += 1.0
+      print_view_position()
       GLUT.PostRedisplay()
     when 'z'
+      @view_from_z -= 10.0
+      print_view_position()
+      GLUT.PostRedisplay()
+    when 'Z'
       @view_from_z -= 1.0
+      print_view_position()
+      GLUT.PostRedisplay()
+    
+    when 'H'
+      @view_to_x -= 1.0
+      print_view_position()
+      GLUT.PostRedisplay()
+    when 'L'
+      @view_to_x += 1.0
+      print_view_position()
+      GLUT.PostRedisplay()
+    when 'K'
+      @view_to_y += 1.0
+      print_view_position()
+      GLUT.PostRedisplay()
+    when 'J'
+      @view_to_y -= 1.0
+      print_view_position()
       GLUT.PostRedisplay()
     when '\033' # ESC but It's not \033, and no print ESC
       exit()
@@ -213,20 +353,35 @@ class Sample
   end
 
   def view_from_move_to_init()
-    @view_from_x = 3.0
-    @view_from_y = 4.0
-    @view_from_z = 5.0
+    @view_from_x = 10.0
+    @view_from_y = 10.0
+    @view_from_z = 70.0
+    @view_to_x = 10.0
+    @view_to_y = 10.0
+    @view_to_z = 0.0
+  end
+
+  def print_view_position()
+    puts "view from: #{@view_from_x} #{@view_from_y} #{@view_from_z}"
+    puts "       to: #{@view_to_x} #{@view_to_y} #{0}"
   end
 
   def init()
     @points = []
-    @width = 600.0
-    @height = 400.0
+    @width = 700.0
+    @height = 700.0
     @mouse_on_x = 0
     @mouse_on_y = 0
     @r = 0
     @rotate_flag = false
     view_from_move_to_init()
+    #--------------
+    # temp
+    #--------------
+    puts "test"
+    puts File::open(".config").read()
+    @map = Map.new(File::open(".config").read())
+    @map.read()
 
     GLUT.InitWindowPosition(200, 200)
     GLUT.InitWindowSize(@width, @height)
